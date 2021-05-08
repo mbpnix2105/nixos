23126d6c50f0f4f48458e444830c8cf896ed5238
mbpnix2105 <83704106+mbpnix2105@users.noreply.github.com>
2021-05-08 12:49:04 +0300

    Update nix.nix

--- system/modules/nix.nix
+++ system/modules/nix.nix
@@ -25,7 +25,7 @@
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
-      "mbpnix.cachix.org-1:nAfBijPdJRqcMhwDlIr4LbwPPKVWHROKx02Bcc/WbAI="
+      "mbpnix.cachix.org-1:3n0pVEB+BD0ZapC/Z6BR7yicfCHV2xaiKwAHsQDaL2M="
     ];
     trustedUsers = [ "root" "mbpnix" ];
   };
