bd5969b632a44cffb80a8936f4f986161c87fc5f
mbpnix2105 <mbpnix2105@protonmail.com>
2021-05-09 08:04:42 +0300

    Update cachix

--- system/modules/nix.nix
+++ system/modules/nix.nix
@@ -19,6 +19,7 @@
       "https://nix-community.cachix.org"
       "https://fufexan.cachix.org"
       "https://mbpnix.cachix.org"
+      "https://mbpnix2105.cachix.org"
     ];
     binaryCachePublicKeys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -26,6 +27,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
       "mbpnix.cachix.org-1:3n0pVEB+BD0ZapC/Z6BR7yicfCHV2xaiKwAHsQDaL2M="
+      "mbpnix2105.cachix.org-1:/v6ySV4kYSqaCUW0QDD4bC1xkNYUDEIm6KxuN753vXw="
     ];
     trustedUsers = [ "root" "mbpnix" ];
   };
